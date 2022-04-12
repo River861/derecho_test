7b3cea9a6035664f4c331a45086c630b673ea025
xuchuan <xuchuan@node-1.xuchuan-122718.scalablerpc-pg0.apt.emulab.net>
2022-04-12 06:11:15 -0600

    opt

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ using std::endl;
 
 const int num_clients = 128;           // clients数目
 const int shard_size = 128;            // 也就是replica factor
-const uint64_t num_messages = 100000;  // 发送消息的数目
+const double test_seconds = 10.0;      // 测试时间
 const int msg_size = 1024;             // msg大小
 
 
@@ -100,13 +100,13 @@ int main(int argc, char** argv) {
     // 3. throughput测试逻辑
     group.barrier_sync();
     auto start_time = std::chrono::steady_clock::now();
-    uint64_t cnt = 0;
+    uint64_t cnt = 0, nanoseconds_elapsed;
     do {
         send_one(cnt);
         cnt ++;
-    } while(cnt < num_messages);
-    uint64_t nanoseconds_elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_time).count();
-    double bw = (cnt + 0.0) / nanoseconds_elapsed *1e9;
+        nanoseconds_elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_time).count();
+    } while(nanoseconds_elapsed < test_seconds * 1e9);
+    double bw = (cnt + 0.0) / nanoseconds_elapsed * 1e9;
     double total_bw = aggregate_bandwidth(members_order, members_order[node_rank], bw);
 
     // log the result at the leader node
