f7100d2daab129a922f4c41d142be055ed5ac54f
xuchuan <xuchuan@node-1.xuchuan-122718.scalablerpc-pg0.apt.emulab.net>
2022-04-12 06:30:23 -0600

    debug

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ using std::endl;
 const int num_clients = 24;           // clients数目
 const int shard_size = 24;            // 也就是replica factor
 const double test_seconds = 10.0;      // 测试时间
-const int msg_size = 1024;             // msg大小
+const int msg_size = 10;             // msg大小
 
 
 int main(int argc, char** argv) {
