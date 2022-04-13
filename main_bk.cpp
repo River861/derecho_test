977ffd0e0548bbfdc68693bac65c5c906ce37f2c
xuchuan <xuchuan@node-0.jcshen-123044.scalablerpc-pg0.apt.emulab.net>
2022-04-13 04:47:27 -0600

    debug

--- main_bk.cpp
+++ main_bk.cpp
@@ -35,7 +35,7 @@ const int num_clients = 12;          // clients数目
 const int shard_size = 4;           // 也就是replica factor
 // const double test_time = 10.0;      // 测试时间
 const int msg_size = 1024;
-const int total_msg_num = 10000;
+const int total_msg_num = 100000;
 
 
 int main(int argc, char** argv) {
