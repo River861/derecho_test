19704f7a1209f71fc7b18b0c2746095d8838fc30
xuchuan <xuchuan@node-0.jcshen-123044.scalablerpc-pg0.apt.emulab.net>
2022-04-13 08:44:01 -0600

    debug

--- main.cpp
+++ main.cpp
@@ -71,9 +71,10 @@ int main(int argc, char** argv) {
     auto send_one = [&]() {
         uint64_t new_value = node_rank;
         derecho::rpc::QueryResults<bool> results = rpc_handle.ordered_send<RPC_NAME(change_state)>(new_value);
-        for(auto& reply_pair : results.get()) {
-            reply_pair.second.get();
-        }
+        // bool results_total = true;
+        // for(auto& reply_pair : results.get()) {
+        //     results_total = results_total && reply_pair.second.get();
+        // }
 
         // std::string new_value = std::to_string(node_rank);
         // new_value += std::string(msg_size - new_value.size(), 'x');
