a2e4aae7d034d0a27952c048b151ccf857fb4bd5
xuchuan <xuchuan@node-0.jcshen-123044.scalablerpc-pg0.apt.emulab.net>
2022-04-14 07:38:50 -0600

    debug

--- aggregate_bandwidth.cpp
+++ aggregate_bandwidth.cpp
@@ -7,8 +7,8 @@ double aggregate_bandwidth(std::vector<uint32_t> members, uint32_t node_id,
     OneResultSST sst(sst::SSTParams(members, node_id));
     sst.bw[sst.get_local_index()] = bw;
     sst.put();
-    std::cout << "node: " << sst.get_local_index() << " bw: " << bw << std::endl;
     sst.sync_with_members();
+    std::cout << "node: " << sst.get_local_index() << " bw: " << bw << std::endl;
     double total_bw = 0.0;
     unsigned int num_nodes = members.size();
     for(unsigned int i = 0; i < num_nodes; ++i) {
